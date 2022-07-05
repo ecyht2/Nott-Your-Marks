5866a589361e1bdedbf8fbbf902da94f0169fcde
ecyht2 <ecyht2@nottingham.edu.my>
2022-07-05 16:08:28 +0800

    Made code more "pythonic"

--- nott-your-marks.py
+++ nott-your-marks.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Calculates the average marks for your modules."""
 import argparse
-import csv, _csv
+import csv
+import _csv
 import _io
 
 def parse_arguments():
+    """Create an argparse instance."""
     parser = argparse.ArgumentParser(description='Calculates your average marks',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
                                      epilog='''
@@ -15,8 +18,9 @@ Example: EEEE 1028''')
     return parser.parse_args()
 
 def input_codes(credit) -> list:
+    """Get the module codes using stdin."""
     # Getting Codes
-    codes = list()
+    codes = []
     code = "Dummy"
     while code:
         code = input("Input Module Code (Press Return to Finish): ")
@@ -24,23 +28,25 @@ def input_codes(credit) -> list:
             if code not in credit:
                 print("Invalid Code, Try Again")
             else:
-                if not code in codes:
+                if code not in codes:
                     codes.append(code)
                 else:
                     print("The code given is already passed in, Try Again")
     return codes
 
 def input_marks(codes: list) -> dict:
+    """Get the marks for each modules using stdin."""
     # Getting Marks
-    marks = dict()
+    marks = {}
     for code in codes:
         mark = input(f"Input the mark for module {code}: ")
         marks[code] = int(mark)
     return marks
 
-def get_marks_csv(csvFile: _io.TextIOWrapper) -> dict:
-    reader: _csv.reader = csv.reader(csvFile)
-    marks: dict = dict()
+def get_marks_csv(csv_file: _io.TextIOWrapper) -> dict:
+    """Get the marks and it's corresponding module code from csv_file."""
+    reader: _csv.reader = csv.reader(csv_file)
+    marks: dict = {}
 
     for data in reader:
         if not data[0] in marks:
@@ -52,23 +58,25 @@ def get_marks_csv(csvFile: _io.TextIOWrapper) -> dict:
     return marks
 
 def calculate_marks(marks: dict, credit: dict) -> float:
+    """Calculates the average marks."""
     # Calculating total marks
-    totalCredits: int = 0
-    totalMarks: float = 0
+    total_credits: int = 0
+    total_marks: float = 0
     for module, mark in marks.items():
-        moduleCredit: int = credit[module]
-        totalCredits += moduleCredit
-        totalMarks += moduleCredit * mark
-    totalMarks /= totalCredits
+        module_credit: int = credit[module]
+        total_credits += module_credit
+        total_marks += module_credit * mark
+    total_marks /= total_credits
 
-    return totalMarks
+    return total_marks
 
 def main():
+    """Main function."""
     args = parse_arguments()
 
     # Loading Credits
-    credit = dict()
-    with open('credits.csv', 'r') as csvfile:
+    credit = {}
+    with open('credits.csv', 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for data in reader:
             credit[data["Code"]] = int(data["Credit"])
@@ -82,8 +90,8 @@ def main():
     else:
         marks = get_marks_csv(args.file)
 
-    totalMarks: float = calculate_marks(marks, credit)
-    print("Your Marks:", totalMarks)
+    total_marks: float = calculate_marks(marks, credit)
+    print("Your Marks:", total_marks)
 
 if __name__ == '__main__':
     main()
